cd6a48fb588f5cc10f5aea9a77053ea4caf8a146
Mark Teicher <mark.teicher@gmail.com>
2025-12-16 11:19:19 -0500

    Update sites.py

--- bin/zap_collectors/sites.py
+++ bin/zap_collectors/sites.py
@@ -1,1 +1,25 @@
+# zap_collectors/sites.py
 
+def collect_sites(zap_client):
+    """
+    Collect sites known to ZAP.
+
+    :param zap_client: initialized ZAP API client
+    :return: list of site strings or dicts (API dependent)
+    """
+
+    response = zap_client.get(
+        "/JSON/core/view/sites/"
+    )
+
+    # ZAP returns sites as a list under the "sites" key
+    sites = response.get("sites", [])
+
+    records = []
+
+    for site in sites:
+        records.append({
+            "site": site
+        })
+
+    return records
